5ad5f180fc84526844470ffdc58f4c63490cf925
giahienpham <giahien14604@gmail.com>
2024-09-30 12:22:31 -0400

    add projects session

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
+        <Projects/> 
       </div>
     </div>
   );
