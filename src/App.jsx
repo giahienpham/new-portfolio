09af476c3bef0c12458545ed60b5334813e8f755
giahienpham <giahien14604@gmail.com>
2024-09-30 01:08:06 -0400

    add technologies section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Technologies from "./components/Technologies";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
+        <Technologies/>
       </div>
     </div>
   );
